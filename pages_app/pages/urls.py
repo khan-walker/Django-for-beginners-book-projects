f908e2b27ce497a9ffc2eb2cf3db21a350065295
khan-walker <khanwalkergithub@gmail.com>
2024-04-07 16:26:15 +0500

    add tests

--- pages_app/pages/urls.py
+++ pages_app/pages/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('about', AboutPageView.as_view(), name='about'),
+    path('about/', AboutPageView.as_view(), name='about'),  # Corrected path with a trailing slash
     path('', HomePageView.as_view(), name='home'),
 ]
-
-
